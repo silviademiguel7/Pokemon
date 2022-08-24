7200fbccaff1e11e62f608cdc2e93cea68c63fed
silviademiguel7 <silviademiguel7@gmail.com>
2022-08-24 14:44:03 +0200

    feat: carga los pokemons en una tabla ordenados alfabeticamente

--- src/pages/PokemonList/PokemonList.tsx
+++ src/pages/PokemonList/PokemonList.tsx
@@ -1,5 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getPokemonList, Pokemon } from '../../services/getPokemonList.ts';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export const PokemonList = () => {
-  return <div>Pokemon List</div>;
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const onLoad = async () => {
+      try {
+        const pokemonListResponse = await getPokemonList();
+        setPokemons(pokemonListResponse);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    onLoad();
+  });
+
+  return (
+    <>
+      <h1 className='title'>Pokemon List</h1>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align='right'>URL</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {pokemons.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component='th' scope='row'>
+                  {row.name}
+                </TableCell>
+                <TableCell align='right'>{row.url}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 };
