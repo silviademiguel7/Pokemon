f3e3fbb7b4178e9f7af48ca53b15a712667d155a
silviademiguel7 <silviademiguel7@gmail.com>
2022-08-24 13:57:37 +0200

    feat: inicaliza app con las rutas definidas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { PokemonList } from './pages/PokemonList/PokemonList.tsx';
+import { PokemonDetail } from './pages/PokemonDetail/PokemonDetail.tsx';
 
-function App() {
-  return <div className='App'></div>;
-}
-
-export default App;
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to='/pokemons' replace />} />
+        <Route exact path='/pokemons' element={<PokemonList />} />
+        <Route path='/pokemons/:id' element={<PokemonDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
---
+++ src/pages/PokemonDetail/PokemonDetail.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PokemonDetail = () => {
+  return <div>Pokemon detail</div>;
+};
