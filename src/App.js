134496b29b204707fe7c3762c0ef9bec8c0fd76f
silviademiguel7 <silviademiguel7@gmail.com>
2022-08-25 09:09:53 +0200

    refactor: estila y refactoriza codigo de fltrado

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { PokemonDetail } from './pages/PokemonDetail/PokemonDetail.tsx';
 export const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Navigate to='/pokemons' replace />} />
-        <Route exact path='/pokemons' element={<PokemonList />} />
-        <Route path='/pokemons/:id' element={<PokemonDetail />} />
-      </Routes>
+      <div className='App-wrapper'>
+        <Routes>
+          <Route path='/' element={<Navigate to='/pokemons' replace />} />
+          <Route exact path='/pokemons' element={<PokemonList />} />
+          <Route path='/pokemons/:id' element={<PokemonDetail />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
--- src/pages/PokemonList/PokemonList.tsx
+++ src/pages/PokemonList/PokemonList.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import './PokemonList.css';
 import { getPokemonList, Pokemon } from '../../services/getPokemonList.ts';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -7,10 +8,13 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useNavigate } from 'react-router-dom';
+import ReactLoading from 'react-loading';
 
 export const PokemonList = () => {
+  let navigate = useNavigate();
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-  const [pokemonsFiltered, setPokemonsFiltered] = useState<Pokemon[]>([]);
+  const [filterValue, setFilterValue] = useState<string>('');
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -19,7 +23,6 @@ export const PokemonList = () => {
       try {
         const pokemonListResponse = await getPokemonList();
         setPokemons(pokemonListResponse);
-        setPokemonsFiltered(pokemonListResponse);
       } catch (error) {
         setError(error);
       } finally {
@@ -29,23 +32,40 @@ export const PokemonList = () => {
     onLoad();
   }, []);
 
-  const handleSearchPokemon = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const search = event.target.value.toUpperCase();
-    const filteredPokemons = pokemons.filter((pokemon) =>
-      pokemon.name.toUpperCase().includes(search)
-    );
-    setPokemonsFiltered(filteredPokemons);
+  const handleChangeFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const filterValue = event.target.value.toUpperCase();
+    setFilterValue(filterValue);
   };
-  return (
-    <>
-      <h1 className='title'>Pokemon List</h1>
 
-      <label htmlFor='site-search'>Search pokemons:</label>
-      <input
-        id='site-search'
-        name='site-search'
-        onChange={handleSearchPokemon}
-      />
+  const handleClickPokemon = (url: string) => {
+    const idUrl = url.split('https://pokeapi.co/api/v2/pokemon/')[1];
+    const id = idUrl.split('/')[0];
+    navigate(`/pokemons/${id}`, { replace: true });
+  };
+
+  if (isLoading) {
+    return (
+      <main className='main-content'>
+        <ReactLoading type='balls' color='ffffff' height={66} width={66} />
+      </main>
+    );
+  }
+  if (error) {
+    return <main className='main-content'>Ha habido un error de carga</main>;
+  }
+  return (
+    <main className='main-content'>
+      <h1 className='title'>Pokemons</h1>
+      <div className='search-container__filterInput'>
+        <label htmlFor='inputText'>Search pokemons</label>
+        <input
+          type='text'
+          id='inputText'
+          name='inputText'
+          placeholder='Busca tu pokemon'
+          onChange={handleChangeFilter}
+        />
+      </div>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label='simple table'>
           <TableHead>
@@ -55,20 +75,27 @@ export const PokemonList = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {pokemonsFiltered.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component='th' scope='row'>
-                  {row.name}
-                </TableCell>
-                <TableCell align='right'>{row.url}</TableCell>
-              </TableRow>
-            ))}
+            {pokemons
+              .filter((pokemon) =>
+                pokemon.name.toUpperCase().includes(filterValue)
+              )
+              .map((pokemon) => (
+                <TableRow
+                  key={pokemon.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  onClick={() => {
+                    handleClickPokemon(pokemon.url);
+                  }}
+                >
+                  <TableCell component='th' scope='row'>
+                    {pokemon.name}
+                  </TableCell>
+                  <TableCell align='right'>{pokemon.url}</TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
-    </>
+    </main>
   );
 };
