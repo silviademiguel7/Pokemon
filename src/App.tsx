30215e8770fe77f0e68b3d9298d3d1a382f0fff8
silvia <sperez@izertis.com>
2024-12-16 09:11:22 +0100

    feat: ajust contenedor de aplicacion al centro

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { PokemonList } from './pages/PokemonList/PokemonList';
-import { PokemonDetail } from './pages/PokemonDetail/PokemonDetail';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { PokemonDetail } from "./pages/PokemonDetail/PokemonDetail";
+import { PokemonList } from "./pages/PokemonList/PokemonList";
 
 export const App = () => {
-  return (
-    <BrowserRouter>
-      <div className='App-wrapper'>
-        <Routes>
-          <Route path='/' element={<Navigate to='/pokemons' replace />} />
-          <Route path='/pokemons/:id' element={<PokemonDetail />} />
-          <Route path='/pokemons' element={<PokemonList />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className='App-wrapper'>
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<Navigate
+								to='/pokemons'
+								replace
+							/>
+						}
+					/>
+					<Route
+						path='/pokemons/:id'
+						element={<PokemonDetail />}
+					/>
+					<Route
+						path='/pokemons'
+						element={<PokemonList />}
+					/>
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 };
