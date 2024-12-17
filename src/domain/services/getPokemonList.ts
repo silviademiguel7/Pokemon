25929eb2b65ac52242e284012a8244643f75aa58
silvia <sperez@izertis.com>
2024-12-17 15:04:59 +0100

    chore: add prettier

--- src/domain/services/getPokemonList.ts
+++ src/domain/services/getPokemonList.ts
@@ -6,30 +6,30 @@ import { Pokemon } from "../models/Pokemon";
 export type getPokemosListServiceError = "NoResultsError";
 
 export const getPokemonList = async (): Promise<Pokemon[]> => {
-	const { data } = await apiClient.get<{ results: Pokemon[] }>(
-		"pokemon?limit=11"
-	);
+  const { data } = await apiClient.get<{ results: Pokemon[] }>(
+    "pokemon?limit=11",
+  );
 
-	if (data.results.length === 0) {
-		const serviceError = buildServiceError<getPokemosListServiceError>({
-			message: "No pokemons found",
-			type: "NoResultsError",
-		});
+  if (data.results.length === 0) {
+    const serviceError = buildServiceError<getPokemosListServiceError>({
+      message: "No pokemons found",
+      type: "NoResultsError",
+    });
 
-		throw serviceError;
-	}
+    throw serviceError;
+  }
 
-	return data.results.sort((pokemonA, pokemonB) =>
-		sortAlphabetically(pokemonA.name, pokemonB.name)
-	);
+  return data.results.sort((pokemonA, pokemonB) =>
+    sortAlphabetically(pokemonA.name, pokemonB.name),
+  );
 };
 
 const sortAlphabetically = (nameA: string, nameB: string) => {
-	if (nameA < nameB) {
-		return -1;
-	}
-	if (nameA > nameB) {
-		return 1;
-	}
-	return 0;
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  return 0;
 };
